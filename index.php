47fb01afe52617912b285f505b5b61af5bff0b26
iragaba <58625181+iragaba@users.noreply.github.com>
2022-01-26 10:47:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
   
 <head>
     <title>Storing words</title>
-</head><!DOCTYPE html>
-<html lang="en">
-  
-<head>
-    <title>Storing words</title>
 </head>
 <style>
 a{
@@ -28,7 +23,9 @@ a{
         <h1><center><marquee>KWINJIZAMO AMAGABO MURI DATABASE</marquee></center></h1>
   
         <form  method="post">
-        <?php
+              
+<table bgcolor="powderblue" width="50%" cellspacing="20" border="0">
+<?php
 include "conn.php"; // Using database connection file here
 
 if(isset($_POST['submit']))
@@ -54,59 +51,6 @@ if(isset($_POST['submit']))
 
 mysqli_close($db); // Close connection
 ?>
-              
-<table bgcolor="powderblue" width="50%" cellspacing="20" border="0">
-               <tr> <td><label for="variable">Variable</label></td>
-                <td><input type="text" name="variable" id="var"></td>
-            </tr>
-  
-             <tr>
-               <td> <label for="kinyarwanda">Kinyarwanda</label></td>
-                <td><input type="text" name="kinyarwanda" id="kinya"></td>
-            </tr>
-  
-  <tr>
-               <td> <label for="french">French</label></td>
-               <td> <input type="text" name="french" id="fre"></td>
-            </tr>
-  
-  <tr>              
-                 <td><label for="english">English</label></td>
-               <td> <input type="text" name="english" id="eng"></td>
-                  </tr>
-               <tr>
-                 <td><label for="swahili">Swahili</label></td>
-             <td>   <input type="text" name="swahili" id="swah"></td>
-            </tr>
-   
-           <tr>              
-        <td></td><td><input type="submit" name="submit" value="Submit"> </td></tr>
-            </table>
-        </form>
-    </center>
-</body>
-  
-</html>
-<style>
-a{
-    text-decoration: none;
-    color: black;
-    font-size: 15px;
-    }
-</style>
-
-<body bgcolor="lightgray">
-    <center>
-    <table width="10%" cellspacing="20" border="0">
-<tr>
-    <td><a href="index.php">HOME</a></td><td></td>
-      <td ><a href="new.php"> INSART HERE </a> </td>
-  </tr>
-   
-        <form  method="post">
-              
-              
-<table bgcolor="powderblue" width="50%" cellspacing="20" border="0">
                <tr> <td><label for="variable">Variable</label></td>
                 <td><input type="text" name="variable" id="var"></td>
             </tr>
